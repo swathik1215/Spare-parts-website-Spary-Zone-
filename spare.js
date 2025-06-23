33268f92b2ccf45f4de83a879df54c9a59a4fdfe
swathik1215 <thesde.swathi1215@gmail.com>
2025-06-23 09:10:00 +0530

    2nd update

--- spare.js
+++ spare.js
@@ -1,3 +1,32 @@
 function sendnext(){
     window.location.href = 'servicerequest2.html';
 }
+
+function findplace() {
+    alert("✅ Service is available at your location.");
+}
+function addToCart() {
+    const qty = parseInt(document.getElementById("quantity").value);
+    const product = {
+    name: "NGK Iridium Spark Plug (Set of 4)",
+    part: "BKR6EIX-11",
+    price: 3203,
+    qty: qty
+    };
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    cart.push(product);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    updateCartCount();
+    alert(`${qty} item(s) added to cart!`);
+}
+function buyNow() {
+    const qty = document.getElementById("quantity").value;
+    alert(`Proceeding to checkout with ${qty} item(s).`);
+    window.location.href = `checkout.html?item=NGK+BKR6EIX-11&qty=${qty}`;
+}
+function updateCartCount() {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    let totalQty = cart.reduce((sum, item) => sum + item.qty, 0);
+    document.getElementById("cartCount").textContent = totalQty;
+}
+window.onload = updateCartCount;
